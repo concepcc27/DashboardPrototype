eb989b549137b27dcf6e3f40dcfe8c46b5d2ca3d
Incedendo <nguyenminhkiet94@gmail.com>
2017-07-10 10:40:54 -0500

    axios test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    projects: []
+  };
+
+  componentDidMount() {
+   const dataAPI = 'https://jsonplaceholder.typicode.com';
+   axios.get(dataAPI + '/posts')
+     .then((response) => {
+       this.setState({
+         projects: response.data.slice(0, 12)
+       });
+      console.log(this.state.projects);
+     })
+     .catch( (error) => {
+       console.log(error);
+     }
+   );
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+      <h1>Hello</h1>
       </div>
     );
   }
