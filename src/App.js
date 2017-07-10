e1bf7b29d852b00a7228a4cbb1a07e49fc3bb4df
cconcep <cconcep@purdue.edu>
2017-07-10 15:02:52 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {Display} from './components/Display';
 import './App.css';
 import axios from 'axios';
 import {ChartDisplay} from './components/ChartDisplay'
@@ -10,7 +9,7 @@ class App extends Component {
 		projects: []
 	};
 
-<<<<<<< HEAD
+
   componentDidMount() {
    const dataAPI = 'http://localhost:3000';
    axios.get(dataAPI + '/test')
@@ -26,32 +25,15 @@ class App extends Component {
      }
    );
   }
-=======
-	componentDidMount () {
-	  const dataAPI = 'https://jsonplaceholder.typicode.com';
-	  axios.get(dataAPI + '/posts')
-	    .then((response) => {
-	      this.setState({
-	        projects: response.data.slice(0, 12)
-	      });
-				console.log(response.data);
-	    })
-	    .catch( (error) => {
-        console.log(error);
-	    }
-	  );
-	}
->>>>>>> Dev
+
+
 
   render() {
     return (
 			<div className="wrapper">
-<<<<<<< HEAD
+
 				<h1>Hello</h1>
       <ChartDisplay list={this.state.projects} />
-=======
-        <Display elements={this.state.projects}/>
->>>>>>> Dev
 			</div>
     );
   }
